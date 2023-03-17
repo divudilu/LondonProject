3c6d52f1071bc5b78ac11887a4a987bbb8260564
https://github.com/divudilu/NHSBSA.gitGit <divudilu@gmail.com>
2023-03-17 13:23:40 +0000

    1st commit

---
+++ src/com/london/org/TestClass1.java
@@ -0,0 +1,31 @@
+package com.london.org;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestClass1 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello London");
+		
+		
+		int z,x,y;
+		
+		x=2;
+		y=2;
+		
+		z=x+y;
+		
+		System.out.println(z);
+
+		
+		WebDriver driver=new ChromeDriver();
+		
+		driver.get("https://google.co.in");
+		
+		driver.close();
+
+	}
+
+}
